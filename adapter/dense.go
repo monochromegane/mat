de36d926eaa9177dd7a5b415af8956ff705df846
monochromegane <dev.kuro.obi@gmail.com>
2022-06-21 17:02:27 +0900

    Some func of Dense are adapted.

--- adapter/dense.go
+++ adapter/dense.go
@@ -13,3 +13,27 @@ func DenseCopyOf(a mat.Matrix) *Dense {
 func NewDense(r, c int, data []float64) *Dense {
 	return &Dense{mat.NewDense(r, c, data)}
 }
+
+func (m *Dense) Add(b mat.Matrix) mat.Matrix {
+	var dense mat.Dense
+	dense.Add(m.Dense, b)
+	return &Dense{&dense}
+}
+
+func (m *Dense) Apply(fn func(i, j int, v float64) float64) mat.Matrix {
+	var dense mat.Dense
+	dense.Apply(fn, m.Dense)
+	return &Dense{&dense}
+}
+
+func (m *Dense) Augment(b mat.Matrix) mat.Matrix {
+	var dense mat.Dense
+	dense.Augment(m.Dense, b)
+	return &Dense{&dense}
+}
+
+func (m *Dense) CloneFrom() mat.Matrix {
+	var dense mat.Dense
+	dense.CloneFrom(m.Dense)
+	return &Dense{&dense}
+}
--- adapter/dense_test.go
+++ adapter/dense_test.go
@@ -35,3 +35,78 @@ func TestNewDense(t *testing.T) {
 		t.Errorf("Result of NewDense should be equal.")
 	}
 }
+
+func TestDenseAdd(t *testing.T) {
+	a := mat.NewDense(2, 2, data)
+	b := mat.NewDense(2, 2, data)
+
+	org := mat.NewDense(2, 2, nil)
+	org.Add(a, b)
+
+	ax := DenseCopyOf(a)
+	adapted := ax.Add(b)
+
+	if !mat.EqualApprox(org, adapted.(*Dense).Dense, epsilon) {
+		t.Errorf("Result of Add should be equal.")
+	}
+
+	ax = DenseCopyOf(a)
+	bx := DenseCopyOf(b)
+	adapted = ax.Add(bx)
+
+	if !mat.EqualApprox(org, adapted.(*Dense).Dense, epsilon) {
+		t.Errorf("Result of Add should be equal.")
+	}
+}
+
+func TestDenseApply(t *testing.T) {
+	a := mat.NewDense(2, 2, data)
+	fn := func(i, j int, v float64) float64 { return v * 2.0 }
+
+	org := mat.NewDense(2, 2, nil)
+	org.Apply(fn, a)
+
+	ax := DenseCopyOf(a)
+	adapted := ax.Apply(fn)
+
+	if !mat.EqualApprox(org, adapted.(*Dense).Dense, epsilon) {
+		t.Errorf("Result of Apply should be equal.")
+	}
+}
+
+func TestDenseAugment(t *testing.T) {
+	a := mat.NewDense(2, 2, data)
+	b := mat.NewDense(2, 2, data)
+
+	org := mat.NewDense(2, 4, nil)
+	org.Augment(a, b)
+
+	ax := DenseCopyOf(a)
+	adapted := ax.Augment(b)
+
+	if !mat.EqualApprox(org, adapted.(*Dense).Dense, epsilon) {
+		t.Errorf("Result of Augment should be equal.")
+	}
+
+	ax = DenseCopyOf(a)
+	bx := DenseCopyOf(b)
+	adapted = ax.Augment(bx)
+
+	if !mat.EqualApprox(org, adapted.(*Dense).Dense, epsilon) {
+		t.Errorf("Result of Augment should be equal.")
+	}
+}
+
+func TestDenseCloneFrom(t *testing.T) {
+	a := mat.NewDense(2, 2, data)
+
+	org := mat.NewDense(2, 2, nil)
+	org.CloneFrom(a)
+
+	ax := DenseCopyOf(a)
+	adapted := ax.CloneFrom()
+
+	if !mat.EqualApprox(org, adapted.(*Dense).Dense, epsilon) {
+		t.Errorf("Result of CloneFrom should be equal.")
+	}
+}
